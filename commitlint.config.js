f2d755fa01a263f441d6fd100717285b4e0c81ca
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-02-13 09:41:12 +0100

    chore: set up CI workflow and commit message linting

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'body-leading-blank': [2, 'always']
+  }
+};
